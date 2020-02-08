823068b2f0fafe746fc2445edd16c7c0c6c766d9
GuilhermeS94 <GuilhermeS94@users.noreply.github.com>
2020-02-07 23:27:06 -0300

    operacao de consultar saldo ok, saque 50%, falta salvar transacao.

--- desafio-app/modelos/contamd.js
+++ desafio-app/modelos/contamd.js
@@ -27,18 +27,36 @@ function DepositoEmConta(){
     
 };
 
-function ConsultarSaldoDaConta(idConta, retorno){
+/**
+* Funcao que retorna o saldo da conta
+* @param {number} idConta ID da conta que deseja ver o saldo
+* @return {Promise} Promise com sucesso ou erro da operacao
+*/
+function ConsultarSaldoDaConta(idConta){
     return sql.connect(config).then(pool => {
         
         return pool.request()
             .input("id_conta", sql.Int, idConta)
-            .query("select * from Contas where IdConta = @id_conta");
+            .query("SELECT Saldo FROM Contas WHERE IdConta = @id_conta");
     
     });
 };
 
-function SacarDaConta(){
+/**
+* Funcao que salva transacao e atualiza saldo da conta
+* @param {number} idConta ID da conta que deseja ver o saldo
+* @param {number} valorSaque Valor que deseja sacar
+* @return {Promise} Promise com sucesso ou erro da operacao
+*/
+function SacarDaConta(idConta, valorSaque){
+    return sql.connect(config).then(pool => {
+        
+        return pool.request()
+            .input("id_conta", sql.Int, idConta)
+            .input("valor_saque", sql.Decimal, valorSaque)
+            .query("UPDATE Contas SET Saldo = (Saldo - @valor_saque) WHERE IdConta = @id_conta");
     
+    });
 };
 
 function ExtratoDeTransacoesDaConta(){
--- desafio-app/rotas/conta.js
+++ desafio-app/rotas/conta.js
@@ -14,9 +14,30 @@ roteador.post("/nova-conta", (req, res)=>{
 roteador.get("/consultar-saldo/:idconta", (req, res)=>{
     
     conta.ConsultarSaldoDaConta(req.params.idconta).then(retorno => {
-        res.status(200).send(retorno.recordset);
+        var saldo = {Saldo:0};
+        console.log(retorno);
+        
+        if(retorno.recordset.length > 0) saldo = retorno.recordset[0];
+        else res.status(204).json(saldo);
+
+        res.status(200).json(saldo);
     }).catch(err => {
-        res.status(500).send(err);
+        res.status(500).json(err);
+    });;
+    
+});
+
+roteador.put("/sacar", (req, res)=>{
+    
+    conta.SacarDaConta(req.body.idconta, req.body.saque).then(retorno => {
+        var sucesso = {Efetuado:false};
+        console.log(retorno);
+        
+        sucesso.Efetuado = retorno.rowsAffected[0] == 1;
+
+        res.status(200).json(sucesso);
+    }).catch(err => {
+        res.status(500).json(err);
     });;
     
 });
